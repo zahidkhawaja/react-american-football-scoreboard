11eb888b5187bd450a62fbe0408dbf924980d8a7
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-02-17 16:31:08 -0500

    Add timer countdown functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //TODO: STEP 1 - Import the useState hook.
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
@@ -7,6 +7,11 @@ function App() {
   //TODO: STEP 2 - Establish your application's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homeScore, setHomeScore] = useState(0);
   const [awayScore, setAwayScore] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(9);
+
+
+const deductSeconds = () => seconds > 0 ? setSeconds(seconds -1) : null;
 
   return (
     <div className="container">
@@ -19,7 +24,7 @@ function App() {
 
             <div className="home__score">{homeScore}</div>
           </div>
-          <div className="timer">00:03</div>
+          <div onLoad = { setTimeout(deductSeconds, 1000) } className="timer">0{minutes}:0{seconds}</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
             <div className="away__score">{awayScore}</div>
