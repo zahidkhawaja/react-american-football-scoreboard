4e7be730381a76ce6cd3e7b7dc89aeecbfa0172d
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-02-17 19:05:49 -0500

    Optimize state variable calls

--- src/App.js
+++ src/App.js
@@ -36,12 +36,12 @@ const deductSeconds = () => seconds > 0 ? setSeconds(seconds -1) : null;
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button onClick = { () => setHomeScore(homeScore + 7)} className="homeButtons__touchdown">Home Touchdown</button>
-          <button onClick = { () => setHomeScore(homeScore + 3)} className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button onClick = { () => setHomeScore(currentValue => currentValue + 7)} className="homeButtons__touchdown">Home Touchdown</button>
+          <button onClick = { () => setHomeScore(currentValue => currentValue + 3)} className="homeButtons__fieldGoal">Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button onClick = { () => setAwayScore(awayScore + 7)} className="awayButtons__touchdown">Away Touchdown</button>
-          <button onClick = { () => setAwayScore(awayScore + 3)} className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button onClick = { () => setAwayScore(currentValue => currentValue + 7)} className="awayButtons__touchdown">Away Touchdown</button>
+          <button onClick = { () => setAwayScore(currentValue => currentValue + 3)} className="awayButtons__fieldGoal">Away Field Goal</button>
         </div>
       </section>
     </div>
